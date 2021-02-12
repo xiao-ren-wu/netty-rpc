aee04a116f3724803206849e5e08e9f344fc4ed8
xiao-ren-wu <18629015421@163.com>
2021-02-12 13:55:02 +0800

    init

---
+++ rpc-test/src/main/java/org/ywb/rpc/test/RpcTestApplication.java
@@ -0,0 +1,14 @@
+package org.ywb.rpc.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yuwenbo
+ */
+@SpringBootApplication
+public class RpcTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RpcTestApplication.class, args);
+    }
+}
---
+++ rpc-test/src/test/java/org/ywb/rpc/test/RpcTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ywb.rpc.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
